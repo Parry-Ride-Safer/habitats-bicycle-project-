43be327249903777c79468a6e42485b23af72024
David <davidjspinto92@gmail.com>
2021-12-17 09:17:20 +0000

    Auto stash before merge of "Dev" and "market-test"

---
+++ Client/src/Components/Map/Map.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import {GoogleMap, InfoWindow, Marker } from '@react-google-maps/api';
+import { formatRelative } from 'date-fns';
+import { useGlobalMapContext } from '../../Context/MapContext';
+
+const mapContainerStyle = {
+  width: "100vw",
+  height: "100vh",
+}
+
+const center = {
+  lat: 52.5200,
+  lng: 13.4050,
+}
+
+const options = {
+  styles: [
+    {
+      featureType: "poi",
+      elementType: "all",
+      stylers: [
+        {
+          visibility: "off",
+        },
+      ],
+    },
+  ],
+  disableDefaultUI:true,
+  zoomControl:true
+}
+
+export default function Map () {
+    const {markers, onMapClick, onMapLoad, selected, setSelected} = useGlobalMapContext();
+  
+    return(
+      <GoogleMap 
+        mapContainerStyle={mapContainerStyle}
+        zoom = {8}
+        center = {center}
+        options = {options}
+        onClick = {onMapClick}
+        onLoad={onMapLoad}
+        >    
+      {markers.map((marker) => (
+        <Marker 
+          key={marker.time} 
+          position={{lat: marker.lat, lng: marker.lng}}
+          /*we can use the prop "icon" to change the icon according to our event.
+          usar o seguinte código para o icon aparecer no sítio certo:
+          scaledSize: new window.google.maps.Size(30, 30),
+          origin: new window.google.maps.Point(0,0);
+          anchor: new window.google.maps.Point(15, 15)
+          */
+        onClick={()=>{setSelected(marker)}}
+        />
+ 
+        ))}
+        {selected  ? (<InfoWindow position = {{lat: selected.lat, lng: selected.lng}}
+         onCloseClick={() => 
+          {setSelected(null)}}>
+          <div>
+            <p>Categorias</p>
+            <ul>
+              <li>Problem 1</li>
+              <li>Problem 2</li>
+              <li>Problem 3</li>
+            </ul>
+            <p>{formatRelative(selected.time, new Date())}</p>
+          </div>
+        </InfoWindow>) : null}
+      </GoogleMap>  
+    )
+}
+
