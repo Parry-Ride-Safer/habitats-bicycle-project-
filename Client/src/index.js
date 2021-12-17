da899d2867c5b576cad8039ceb6857a4f48ef029
hugosylva <hugosylva@hotmail.com>
2021-12-17 09:41:10 +0000

    1st version of login form

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import FormComponent from "./Components/Login/index";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <FormComponent/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
