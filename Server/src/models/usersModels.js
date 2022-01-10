6111762cc1769a8b37a66ad44e439d074241a7d6
Paulo <paulocpc2202@gmail.com>
2022-01-10 16:58:07 +0000

    error handling

--- Server/src/models/usersModels.js
+++ Server/src/models/usersModels.js
@@ -13,108 +13,71 @@ connection.connect((error) => {
 });
 
 const getUsers = async () => {
-  try {
-    const users = await db.query("SELECT email from users");
-    return users[0];
-  } catch (error) {
-    console.log(error);
-  }
+  const users = await db.query("SELECT email from users");
+  return users[0];
 };
 
 const createUser = async ({ password, ...body }) => {
-  try {
-    const hashedPassword = await hashPassword(password, saltedRounds);
-    const [rawResults] = await db.query(
-      "INSERT INTO users (email, hashedPassword)  VALUES (?, ?)",
-      [body.email, hashedPassword]
-    );
-    const id = rawResults.insertId;
-    return { id, ...body };
-  } catch (error) {
-    console.log(error);
-  }
+  const hashedPassword = await hashPassword(password, saltedRounds);
+  const [rawResults] = await db.query(
+    "INSERT INTO users (email, hashedPassword)  VALUES (?, ?)",
+    [body.email, hashedPassword]
+  );
+  const id = rawResults.insertId;
+  return { id, ...body };
 };
 
 const validateEmail = async (email) => {
-  try {
-    const [results] = await db.query(
-      "SELECT id, email FROM users WHERE email=?",
-      [email]
-    );
-    return results[0];
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
+  const [results] = await db.query(
+    "SELECT id, email FROM users WHERE email=?",
+    [email]
+  );
+  return results[0];
 };
 
 const findUserbyId = async (userId) => {
-  try {
-    const rawResults = await db.query("SELECT  email FROM users WHERE id = ?", [
-      userId,
-    ]);
-    const [results] = rawResults;
-    return results[0];
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
+  const rawResults = await db.query("SELECT  email FROM users WHERE id = ?", [
+    userId,
+  ]);
+  const [results] = rawResults;
+  return results[0];
 };
 
 const findByEmail = async (email) => {
-  try {
-    const [results] = await db.query("SELECT * FROM users WHERE email = ?", [
-      email,
-    ]);
-    return results[0];
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
+  const [results] = await db.query("SELECT * FROM users WHERE email = ?", [
+    email,
+  ]);
+  return results[0];
 };
 const getUserByEmail = async (email) => {
-  try {
-    let [results] = await db.query(
-      "SELECT id, email, hashedPassword, role FROM users WHERE email= ?;",
-      [email]
-    );
-    return results;
-  } catch (error) {
-    console.log(err);
-    return err;
-  }
+  let [results] = await db.query(
+    "SELECT id, email, hashedPassword, role FROM users WHERE email= ?;",
+    [email]
+  );
+  return results;
 };
 
 const updateUser = async ({ password, ...data }, id) => {
   let results;
-  try {
-    if (password) {
-      const hashedPassword = await hashPassword(password, saltedRounds);
-      results = await db.query("UPDATE users SET ? WHERE id=?;", [
-        { ...data, hashedPassword },
-        id,
-      ]);
-    } else {
-      results = await db.query("UPDATE users SET ? WHERE id=?;", [
-        { ...data },
-        id,
-      ]);
-    }
-    return results[0].affectedRows;
-  } catch (error) {
-    console.log(error);
-    return error;
+
+  if (password) {
+    const hashedPassword = await hashPassword(password, saltedRounds);
+    results = await db.query("UPDATE users SET ? WHERE id=?;", [
+      { ...data, hashedPassword },
+      id,
+    ]);
+  } else {
+    results = await db.query("UPDATE users SET ? WHERE id=?;", [
+      { ...data },
+      id,
+    ]);
   }
+  return results[0].affectedRows;
 };
 
 const deleteUser = async (id) => {
-  try {
-    let [results] = await db.query("DELETE FROM users WHERE id = ?", [id]);
-    return results;
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
+  let [results] = await db.query("DELETE FROM users WHERE id = ?", [id]);
+  return results;
 };
 
 module.exports = {
