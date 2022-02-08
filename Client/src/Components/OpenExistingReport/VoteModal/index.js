ef6819e2c07d9c587de823a06feb87ecfac20d99
David <davidjspinto92@gmail.com>
2022-02-08 16:50:08 +0000

    merge

--- Client/src/Components/OpenExistingReport/VoteModal/index.js
+++ Client/src/Components/OpenExistingReport/VoteModal/index.js
@@ -12,9 +12,11 @@ export default function ModalShowReportDetails() {
     <div className="vote-layout">
       <div className="edit-vote-display">
         <p>{
-          (parseInt(getReportData.voting) === 1) ? (<div>{dangerLevel[0].icon} </div>) : 
-          (parseInt(getReportData.voting) === 2) ? (<div>{dangerLevel[1].icon} </div>) :
-          (<div>{dangerLevel[2].icon} </div>)
+             ((Number(getReportData.voting).toFixed(2) <=1.29) ? (<div>{dangerLevel[0].icon} </div>) :
+             (
+               (Number(getReportData.voting).toFixed(2) >= 1.30) && 
+               (Number(getReportData.voting).toFixed(2) <=2.29) 
+             ) ?  (<div>{dangerLevel[1].icon} </div>) :  (<div>{dangerLevel[2].icon} </div>))
         }</p>
         <div className="layout-edit-window">
           <p className="danger-title">{
@@ -26,9 +28,11 @@ export default function ModalShowReportDetails() {
           {(currentUser === getReportData.user_id ||
                 (currentUser !== getReportData.user && findReportID)) ?
                 <span className="visitor-vote">including mine{
-                  (parseInt(getReportData.userVoteFound.voting) === 1) ? (<div className="danger-icon-size">{dangerLevel[0].icon} </div>) : 
-                  (parseInt(getReportData.userVoteFound.voting) === 2) ? (<div  className="danger-icon-size">{dangerLevel[1].icon} </div>) :
-                  (<div className="danger-icon-size">{dangerLevel[2].icon} </div>)
+                  ((Number(getReportData.voting).toFixed(2) <=1.29) ? (<div>{dangerLevel[0].icon} </div>) :
+                  (
+                    (Number(getReportData.voting).toFixed(2) >= 1.30) && 
+                    (Number(getReportData.voting).toFixed(2) <=2.29) 
+                  ) ?  (<div>{dangerLevel[1].icon} </div>) :  (<div>{dangerLevel[2].icon} </div>))
                 }</span> : ""}   
           </p>
         </div>
