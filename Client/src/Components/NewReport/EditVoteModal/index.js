8ffb1a278efa19dfd29a3cb9452d262c4629d872
David <davidjspinto92@gmail.com>
2022-02-10 12:41:03 +0000

    fixing sizes

--- Client/src/Components/NewReport/EditVoteModal/index.js
+++ Client/src/Components/NewReport/EditVoteModal/index.js
@@ -33,11 +33,11 @@ export default function ModalShowUserInputDetails() {
           <p className="rating-qtt">
             {getReportData.count} ratings || including mine:
             {Number(findReportID.voting) === 1 ? (
-              <div>{dangerLevel[0].icon} </div>
+              <div className="my-vote-icon">{dangerLevel[0].icon} </div>
             ) : Number(findReportID.voting) === 2 ? (
-              <div>{dangerLevel[1].icon} </div>
+              <div className="my-vote-icon">{dangerLevel[1].icon} </div>
             ) : (
-              <div>{dangerLevel[2].icon} </div>
+              <div className="my-vote-icon">{dangerLevel[2].icon} </div>
             )}
           </p>
         </div>
