493b50d4d8543cf27a1c9677ce42394096b34ac7
Paulo Costa <87874976+Pcpc2202@users.noreply.github.com>
2022-02-23 16:11:22 +0000

    Error Handling Location

--- Server/src/controllers/locationController.js
+++ Server/src/controllers/locationController.js
@@ -4,7 +4,7 @@ const { RecordNotFoundError } = require("../error-types");
 const getAllLocationsController = async (_req, res, next) => {
   try {
     const locations = await locationModels.getAllLocations();
-    if (!locations) throw new RecordNotFoundError("No locations Saved");
+    if (locations.length === 0) throw new RecordNotFoundError("No locations Saved");
     res.status(200).json(locations);
   } catch (error) {
     console.log(error);
